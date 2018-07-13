459ed374be7ba86e82cc20242784b6bfc665ef5f
sn3m <snak3.m@gmail.com>
2018-07-13 19:12:35 +0200

    embeded replay player

--- src/components/views/Replay.js
+++ src/components/views/Replay.js
@@ -4,18 +4,29 @@ class Replay extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      replayFile: "sd",
-      showButtons: true,
-      speed: 1
+      speed: 1,
+      loaded: false
     }
 
   }
-  onPlay = (fileName) => {
-    this.setState({
-      replayFile: fileName,
-      showButtons: false
-    });
 
+  componentDidMount = () => {
+    this.checkAndRun()
+  }
+
+  checkAndRun = () => {
+    if(window.liaGame){
+      window.liaGame.playReplay(
+        "gameView",
+        "assets/",
+        "assets/replays/replay_1.lia",
+        "timeSpinner",
+        "speedSpinner",
+        ["camera1", "camera2", "camera3"]
+      );
+    } else {
+      setTimeout(this.checkAndRun, 100);
+    }
   }
 
   onSpeedChange = (event) => {
@@ -32,26 +43,14 @@ class Replay extends Component {
                 <span id="speedValue" className="normal-text">{this.state.speed + "x"}</span>
             </div>
             <div className="col-sm-1 normal-text">Time:</div>
-            <div className="col-sm-6" id="timeCol"></div>
+            <div className="col-sm-6">
+              <input type="range" min="0" max="0" step="0.01" id="timeSpinner" className="slider" />
+            </div>
             <div className="col-sm-1 cameras">
                 <button type="button" id="camera1">c1</button>
                 <button type="button" id="camera2">c2</button>
                 <button type="button" id="camera3">c3</button>
             </div>
-        </div>
-        <div className="row">
-          {this.state.showButtons ? (
-            <div className="col-sm-12" style={{textAlign: "center"}}>
-                <button type="button" id="replay1" onClick={() => this.onPlay("replay_1.lia")}>Close one</button>
-                <button type="button" id="replay2" onClick={() => this.onPlay("replay_2.lia")}>Dominated</button>
-                <button type="button" id="replay3" onClick={() => this.onPlay("replay_3.lia")}>Noob vs pro</button>
-                <span hidden id="replayFile">sd</span>
-            </div>
-          ) : (
-            <div className="col-sm-12" style={{textAlign: "center"}}>
-                <span hidden id="replayFile">sd</span>
-            </div>
-          )}
         </div>
         <div className="row">
           <div id="gameView"></div>
