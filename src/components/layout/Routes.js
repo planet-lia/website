39775413ea0ef2267e905c6878f95365f30f378d
svigelj <aljaz.svigelj@gmail.com>
2018-12-04 10:39:58 +0100

    A way to turn off editor tracking added

--- src/components/layout/Routes.js
+++ src/components/layout/Routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import LeaderboardPage from '../pages/LeaderboardPage';
 import GamesPage from '../pages/GamesPage';
+import EditorTracking from "../pages/EditorTracking";
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
       <Route exact path="/" component={LandingPage} />
       <Route path="/leaderboard" component={LeaderboardPage} />
       <Route path="/games" component={GamesPage} />
+      <Route path="/editor/tracking" component={EditorTracking} />
     </Switch>
   );
 }
