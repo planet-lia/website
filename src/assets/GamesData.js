9a23977a2b563ac4b89213bb4cf970f39b163bde
sn3m <snak3.m@gmail.com>
2018-12-13 16:20:16 +0100

    Games data change

--- src/assets/GamesData.js
+++ src/assets/GamesData.js
@@ -1,24 +1,24 @@
 export default [{
     gameNum: 1,
     date: "20.06.2018",
-    player1: "r0gue",
-    player2: "RickSanchez",
+    player1: "Ninja7",
+    player2: "SuperGit",
     result: 1,
     duration: 191,
     unitsRemain: "12%"
 }, {
     gameNum: 2,
     date: "19.06.2018",
-    player1: "clone",
-    player2: "NumberSix",
+    player1: "recursive",
+    player2: "TheStudent",
     result: 2,
     duration: 146,
     unitsRemain: "42%"
 }, {
   gameNum: 3,
   date: "18.06.2018",
-  player1: "Forest",
-  player2: "Phenox",
+  player1: "recursive",
+  player2: "TheStudent",
   result: 1,
   duration: 191,
   unitsRemain: "12%"
