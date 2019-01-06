b0ee8340b62b57e83ebba03f03d5ea5ce31f6ae7
svigelj <aljaz.svigelj@gmail.com>
2019-01-06 20:08:45 +0100

    Changed Start playing link to prod docs

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -31,7 +31,7 @@ class LandingPage extends Component {
   }
 
   redirectToGettingStarted = () => {
-    window.open("https://docs-dev.liagame.com/getting-started/", "_blank");
+    window.open("https://docs.liagame.com/getting-started/", "_blank");
   }
 
   onPopupClose = () => {
