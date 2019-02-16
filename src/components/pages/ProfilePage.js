ebf4fbc1329ce024ce905983aaf25978ffd8f38b
sn3m <snak3.m@gmail.com>
2019-02-16 16:18:03 +0100

    Challenge: get constants from backend

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import ReactPaginate from 'react-paginate';
 import GamesTable from '../elems/GamesTable';
 import Moment from 'react-moment';
