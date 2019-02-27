20306c0aca6faee6cdf5302f2a7d8aeffdd22ac1
sn3m <snak3.m@gmail.com>
2019-02-27 12:10:09 +0100

    Brackets: Started

---
+++ src/components/finals/BracketsPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BracketsPage extends Component {
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default BracketsPage
